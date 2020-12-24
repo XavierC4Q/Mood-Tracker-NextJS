112888fa0083e0a1ef8d5e0c7a2caa0750c30a88
Xavier Munroe <munroexavier@gmail.com>
2020-12-24 18:43:56 -0500

    signin page basic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,24 @@
-import {NextComponentType} from 'next';
-import {Provider} from 'next-auth/client';
-import '../styles/globals.scss';
+import { NextComponentType } from "next";
+import { Provider } from "next-auth/client";
+import Head from "next/head";
+import "../styles/globals.scss";
 
-function MyApp({Component, pageProps}: {Component: NextComponentType; pageProps: any}) {
-	return (
-		<Provider session={pageProps.session}>
-			<Component {...pageProps} />
-		</Provider>
-	);
+function MyApp({
+  Component,
+  pageProps,
+}: {
+  Component: NextComponentType;
+  pageProps: { [key: string]: any };
+}) {
+  return (
+    <Provider session={pageProps.session}>
+      <Head>
+        <title>Mood Tracker</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import {providers, signIn} from 'next-auth/client';
+import React from "react";
+import { providers, signIn } from "next-auth/client";
 
-export default function SignIn({providers}: {providers: {[key: string]: {name: string; id: string}}}) {
-	const googleProvider = providers.google;
-	return (
-		<div>
-			<h2>Welcome Back!</h2>
-			<div>
-				<button onClick={() => signIn(googleProvider.id)}>Sign in with {googleProvider.name}</button>
-			</div>
-		</div>
-	);
+interface IProviders {
+  providers: { [key: string]: { name: string; id: string } };
+}
+
+export default function SignIn({ providers }: IProviders) {
+  const googleProvider = providers.google;
+  return (
+    <div>
+      <h2>Welcome Back!</h2>
+      <div>
+        <button onClick={() => signIn(googleProvider.id)}>
+          Sign in with {googleProvider.name}
+        </button>
+      </div>
+    </div>
+  );
 }
 
 SignIn.getInitialProps = async () => {
-	return {
-		providers: await providers()
-	};
+  return {
+    providers: await providers(),
+  };
 };
