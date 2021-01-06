aa6581496a3faee32edb40ee90e58345fbd5194e
mon33k <moniquemojica@pursuit.org>
2021-01-05 20:12:13 -0500

    added favicon and minimal styling in signin page component

--- pages/auth/signin.tsx
+++ pages/auth/signin.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { providers, signIn } from "next-auth/client";
+import styles from "../../styles/Signin.module.scss"
+
+import {useRouter} from 'next/router';
 
 interface IProviders {
   providers: { [key: string]: { name: string; id: string } };
@@ -7,13 +10,17 @@ interface IProviders {
 
 export default function SignIn({ providers }: IProviders) {
   const googleProvider = providers.google;
+  const router = useRouter()
   return (
-    <div>
-      <h2>Welcome Back!</h2>
-      <div>
-        <button onClick={() => signIn(googleProvider.id)}>
+    <div className={styles['signin-container']}>
+      <h2 className={styles['signin-header']}>Welcome Back!</h2>
+      <div className={styles['form-container']}>
+        <button className={styles['signin-button']} onClick={() => signIn(googleProvider.id)}>
           Sign in with {googleProvider.name}
         </button>
+          <button className={styles['back-btn']} onClick={() => router.push('/')}>
+            Back
+          </button>
       </div>
     </div>
   );
